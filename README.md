0a5b024085dd90393c9590ed18820b308a9a4113
Leandro E. Colombo Viña <lecovi@zenbook>
2020-05-21 15:29:36 -0300

    Adds Dockerfile

--- README.md
+++ README.md
@@ -14,3 +14,16 @@ You can use `pipenv` if you prefer.
 ```bash
 poetry run flask run --host=0.0.0.0
 ```
+
+### Docker
+
+Build:
+
+```bash
+docker build . -t verbose-fiesta:0.0.2
+```
+
+Run:
+```bash
+docker run --rm --name verbose-fiesta -v $PWD:/usr/src/app -p 5000:5000 verbose-fiesta:0.0.2
+```
--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from dotenv import load_dotenv
 from flask import Flask
 
@@ -5,9 +7,12 @@ from flask import Flask
 load_dotenv()
 
 
+__version__ = os.environ['VERSION']
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return f'Hello, World! v{__version__}'
